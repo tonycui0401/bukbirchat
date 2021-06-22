f9e00f8f5ea7a8c1371e2438090d45275e335c3c
tonycui0401 <74768915+sosure0401@users.noreply.github.com>
2021-06-22 01:11:13 +0100

    update

--- configs/config.js
+++ configs/config.js
@@ -4,12 +4,12 @@
 module.exports = {
 
   // local_endpoint:'http://localhost:5000/chat',
-  local_endpoint:'http://reecho-env.eba-bk9ugpha.eu-west-1.elasticbeanstalk.com/chat',
+  local_endpoint:'http://bookbirdnew-env.eba-5pwmyms6.eu-west-2.elasticbeanstalk.com/chat',
 
 
   // local_base:"http://localhost:5000",
 
-  local_base:'http://reecho-env.eba-bk9ugpha.eu-west-1.elasticbeanstalk.com',
+  // local_base:'http://reecho-env.eba-bk9ugpha.eu-west-1.elasticbeanstalk.com',
 
 
   // remote_endpoint:'http://reecho-env.eba-bk9ugpha.eu-west-1.elasticbeanstalk.com/chat',
